796cc68aebed07e8836695cb8ca2f53c96eee7b2
shun <w2310751769@gmail.com>
2022-05-20 08:31:19 +0000

    fixed bug

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 import logging
 from main_config import parse_args
 
+logger = logging.getLogger(__name__)
+
 def main():
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
 
-    args = parse_args.args
+    parser = parse_args()
+    args = parser.args
     logger.info(vars(args))
 
-if __init__ == '__main__':
+if __name__ == '__main__':
     main()
--- main_config.py
+++ main_config.py
@@ -1,16 +1,16 @@
 # -*- conding: utf-8 -*-
 import argparse
 
-class parse_args():
+class parse_args:
     def __init__(self):
         self.args = self.init_parse()
         
-    def init_parse():
+    def init_parse(self):
         parser = argparse.ArgumentParser()
 
         # Required parameters
         parser.add_argument('--dataset_name', type=str, default='vua20',
                             choices=['vua18','vua20', 'trofi', 'mohx'],
                             help='name of dataset.')
-
+        
         return parser.parse_args()
